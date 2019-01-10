73ab50eb7e707405a042ab62ff80f385a6eb1570
Driver Station <bhsrobotix2876-programming@gmail.com>
2019-01-09 20:36:50 -0500

    Initial commit

---
+++ src/main/java/org/usfirst/frc2876/PowerUp2018/commands/AutoArmDown.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc2876.PowerUp2018.commands;
+
+import org.usfirst.frc2876.PowerUp2018.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutoArmDown extends Command {
+
+	int counter = 0;
+	
+    public AutoArmDown() {
+        // Use requires() here to declare subsystem dependencies
+    	requires(Robot.intake);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.intake.armDown(0.5);
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	counter++;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return counter >= 200;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.intake.armUp(-0.05);
+    	//Holds arm in place at 90 degree angle
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
---
+++ src/main/java/org/usfirst/frc2876/PowerUp2018/commands/AutoDriveStraightDistance.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc2876.PowerUp2018.commands;
+
+import org.usfirst.frc2876.PowerUp2018.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutoDriveStraightDistance extends Command {
+
+	private double m_distance;
+	private boolean hasMoved = false;
+	
+    public AutoDriveStraightDistance(double distance) {
+    	m_distance = distance;
+    	// Use requires() here to declare subsystem dependencies
+        requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	//Robot.driveTrain.setBrakeMode(true);
+    	Robot.driveTrain.startDistance(m_distance);
+    	Robot.driveTrain.startStraight();
+    	Robot.oi.putSmartDashboardDriveCommand(this);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveTrain.setVelocityStraightDistance();
+    	if(Robot.driveTrain.navxIsMoving()){
+    		hasMoved = true;
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.driveTrain.isDistanceDone() || (!Robot.driveTrain.navxIsMoving() && hasMoved);
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.stopDistance();
+    	Robot.driveTrain.stopStraight();
+    	//Robot.driveTrain.setBrakeMode(false);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
