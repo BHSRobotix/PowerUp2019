f70783e6aa96f1cc2767bd22a87e4bd1786990bd
Maciej Wiczynski <loafjunk@loafdog.org>
2019-01-25 16:18:11 -0500

    Create empty Vector class for student to fill in.

---
+++ src/main/java/org/usfirst/frc2876/PowerUp2018/Pixy2/Pixy2Vector.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc2876.PowerUp2018.Pixy2;
+public class Pixy2Vector {
+    // TODO fill out implementation based on:
+    // https://github.com/charmedlabs/pixy2/blob/2adc6caba774a3056448d0feb0c6b89855a392f4/src/host/arduino/libraries/Pixy2/Pixy2Line.h#L49
+
+    // Hints
+    // - use String.format to replace sprintf
+    // - Make everything public, don't make get/set methods.
+    // - use int instead of uint8_t type
+    
+}
